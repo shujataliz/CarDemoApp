9151d4811116e82e75df04442824f1521849ce43
Shujat Ali <Shujat.dev@gmail.com>
2023-10-31 18:23:59 -0500

    Initial Commit

---
+++ CarAssignment/CarAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAssignmentApp.swift
+//  CarAssignment
+//
+//  Created by Shujat Ali on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
